53ff9c891babc8765d58162aa2c9b72e73dc2295
Yongy-vers <Yongy-vers@outlook.com>
2021-04-07 15:07:31 +0800

    new file:   bool.h	introduce boolean to C 	modified:   list.h	first version for list.h 	modified:   stack.h	todo

---
+++ src/C/STL_C/bool.h
@@ -0,0 +1,7 @@
+/*	bool.h -- introduce boolean value to C		*/
+#ifndef _BOOL_H
+#define _BOOL_H
+
+typedef enum{false, true}bool;
+
+#endif
--- src/C/STL_C/stack.h
+++ src/C/STL_C/stack.h
@@ -1,1 +1,20 @@
 /*	stack.h -- declarations for stack	*/
+
+#ifndef _STACK_H
+#define _STACK_H
+
+#ifndef _BOOL_H
+#include"bool.h"
+#endif
+
+
+
+
+
+
+
+
+	//functions prototype
+
+
+#endif
